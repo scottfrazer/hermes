e18143e8471a2984248c54d4eeeffdad77eeab66
Scott Frazer <scott.d.frazer@gmail.com>
2011-09-28 09:56:56 -0400

    wip

--- hermes/GrammarAnalyzer.py
+++ hermes/GrammarAnalyzer.py
@@ -1,39 +1,53 @@
-import json
+import json, sys
 from hermes.Morpheme import NonTerminal
+
+class Stylizer:
+  pass
+
+class NullStylizer(Stylizer):
+  pass
+
+class AnsiStylizer(Stylizer):
+  pass
 
 class GrammarAnalyzer:
   def __init__(self, grammar):
     self.grammar = grammar
-  def analyze( self, format='human' ):
-    print(" -- Terminals --")
-    print(', '.join([str(e) for e in sorted(self.grammar.terminals, key=lambda x: x.id)]) + "\n")
-    print(" -- Non-Terminals --")
-    print(', '.join([str(e) for e in sorted(self.grammar.nonterminals, key=lambda x: x.id)]) + "\n")
-    print(" -- Normalized Grammar -- ")
-    print("\n".join([ str(r) for r in sorted(self.grammar.getNormalizedRules(), key=lambda x: x.id)]) + "\n")
+  def analyze( self, format='human', stylizer=None, file=sys.stdout ):
+    if stylizer == None:
+      stylizer = NullStylizer()
+    if not isinstance(stylizer, Stylizer):
+      raise Exception('bad stylizer')
+
+    file.write(" -- Terminals --\n")
+    file.write(', '.join([str(e) for e in sorted(self.grammar.terminals, key=lambda x: x.id)]) + "\n\n")
+    file.write(" -- Non-Terminals --\n")
+    file.write(', '.join([str(e) for e in sorted(self.grammar.nonterminals, key=lambda x: x.id)]) + "\n\n")
+    file.write(" -- Normalized Grammar -- \n")
+    file.write("\n".join([ str(r) for r in sorted(self.grammar.getRules(), key=lambda x: x.id)]) + "\n\n")
     
     for grammar in self.grammar.exprgrammars:
-      print(" -- Expression Grammar (%s) -- " % (grammar.nonterminal))
-      print("\n".join([ str(r) for r in sorted(grammar.rules, key=lambda x: x.id)]) + "\n")
+      file.write(" -- Expression Grammar (%s) -- \n" % (grammar.nonterminal))
+      file.write("\n".join([ str(r) for r in sorted(grammar.rules, key=lambda x: x.id)]) + "\n\n")
 
-    print(" -- First sets --")
+    file.write(" -- First sets --\n")
     for N in sorted(self.grammar.first.keys(), key=lambda x: x.id):
       if not isinstance(N, NonTerminal):
         continue
-      print("%s = {%s}" % (N, ', '.join([str(e) for e in self.grammar.first[N]])))
-    print("\n -- Follow sets --")
+      file.write("%s = {%s}\n" % (N, ', '.join([str(e) for e in self.grammar.first[N]])))
+    file.write("\n -- Follow sets --\n")
     for N in sorted(self.grammar.follow.keys(), key=lambda x: x.id):
       if not isinstance(N, NonTerminal):
         continue
-      print("%s = {%s}" % (N, ', '.join([str(e) for e in self.grammar.follow[N]])))
+      file.write("%s = {%s}\n" % (N, ', '.join([str(e) for e in self.grammar.follow[N]])))
     if ( len(self.grammar.warnings) ):
-      print("\n -- Warnings --")
+      file.write("\n -- Warnings --\n")
       for warning in self.grammar.warnings:
-        print('\n' + str(warning))
+        file.write('\n' + str(warning) + '\n')
     if ( len(self.grammar.conflicts) ):
-      print("\n -- Grammar conflicts detected.  Grammar is not LL(1) --")
+      file.write("\n -- Grammar conflicts detected.  Grammar is not LL(1) --\n")
       for conflict in self.grammar.conflicts:
-        print('\n' + str(conflict))
+        file.write('\n' + str(conflict) + '\n')
     else:
-      print("\nGrammar is LL(1)!")
+      file.write("\nGrammar is LL(1)!\n")
   
