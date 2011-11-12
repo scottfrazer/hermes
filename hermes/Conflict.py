b7dd40ea97276b5e5cc875d1ce26680eeb8ad0c9
Scott Frazer <scott.d.frazer@gmail.com>
2011-11-12 11:14:24 -0500

    New unit test format

--- hermes/Conflict.py
+++ hermes/Conflict.py
@@ -17,6 +17,13 @@ class UndefinedNonterminalConflict(Conflict):
   def __init__(self, nonterminal):
     self.__dict__.update(locals())
 
+  def toJson(self):
+    x = [
+      ('type', '"UndefinedNonterminalConflict"'),
+      ('message', 'Nonterminal %s is used but not defined' % (self.nonterminal))
+    ]
+    return '{\n%s\n}' % (',\n'.join(['  "%s": %s' % (k,v) for k,v in x]))
+
   def __str__(self):
     string = ' -- Undefined Nonterminal Conflict-- \n'
     string += 'Nonterminal %s is used but not defined' % (self.nonterminal)
@@ -26,6 +33,14 @@ class ExprConflict(Conflict):
   def __init__( self, terminal, rules ):
     self.terminal = terminal
     self.rules = rules
+  def toJson(self):
+    x = [
+      ('type', '"ExprConflict"'),
+      ('message', '"Terminal %s requires two different %s() functions."' % (self.terminal, self.type))
+    ]
+    for index, rule in enumerate(sorted(self.rules, key=lambda x: str(x))):
+      x.append(('rule-'+str(index), str(rule)))
+    return '{\n%s\n}' % (',\n'.join(['  "%s": %s' % (k,v) for k,v in x]))
   def __str__( self ):
     string = " -- %s conflict -- \n" % (self.type)
     string += "Terminal %s requires two different %s() functions.  Cannot choose between these rules:\n\n"%(self.terminal, self.type)
@@ -45,6 +60,15 @@ class ListFirstFollowConflict(Conflict):
     self.firstNonterminal = firstNonterminal
     self.followList = followList
 
+  def toJson(self):
+    x = [
+      ('type', '"ListFirstFollowConflict"'),
+      ('first(%s)'%(self.listMacro.nonterminal), '[%s]'%(', '.join(sorted([str(e) for e in self.firstNonterminal])))),
+      ('follow(%s)'%(self.listMacro), '[%s]'%(', '.join(sorted([str(e) for e in self.followList])))),
+      ('first(%s) ∩ follow(%s)'%(self.listMacro.nonterminal, self.listMacro), '[%s]'%(', '.join(sorted([str(e) for e in self.firstNonterminal.intersection(self.followList)]))))
+    ]
+    return '{\n%s\n}' % (',\n'.join(['  "%s": %s' % (k,v) for k,v in x]))
+
   def __str__( self ):
     string = " -- LIST FIRST/FOLLOW conflict --\n"
     string += "FIRST(%s) = {%s}\n" %(self.listMacro.nonterminal, ', '.join([str(e) for e in self.firstNonterminal]))
@@ -56,6 +80,19 @@ class FirstFirstConflict(Conflict):
   def __init__( self, rule1, rule2, grammar ):
     self.__dict__.update(locals())
 
+  def toJson(self):
+    rule1_first = self.grammar.ruleFirst(self.rule1)
+    rule2_first = self.grammar.ruleFirst(self.rule2)
+    x = [
+      ('type', '"FirstFirstConflict"'),
+      ('rule-0',  str(self.rule1))
+      ('rule-1',  str(self.rule2))
+      ('first(rule-0)', '[%s]' % (', '.join([str(e) for e in rule1_first])))
+      ('first(rule-1)', '[%s]' % (', '.join([str(e) for e in rule2_first])))
+      ('first(rule-0) ∩ first(rule-1)', '[%s]' % (', '.join(sorted(list(rule1_first.intersection(rule2_first))))))
+    ]
+    return '{\n%s\n}' % (',\n'.join(['  "%s": %s' % (k,v) for k,v in x]))
+
   def __str__( self ):
     rule1_first = self.grammar.ruleFirst(self.rule1)
     rule2_first = self.grammar.ruleFirst(self.rule2)
@@ -63,9 +100,9 @@ class FirstFirstConflict(Conflict):
     string += "Two rules for nonterminal %s have intersecting first sets.  Can't decide which rule to choose based on terminal.\n\n" %(self.rule1.nonterminal)
     string += "(Rule-%d)  %s\n" %(self.rule1.id, self.rule1)
     string += "(Rule-%d)  %s\n\n" %(self.rule2.id, self.rule2)
-    string += "first(Rule-%d) = {%s}\n" %(self.rule1.id, ', '.join([str(e) for e in rule1_first]))
-    string += "first(Rule-%d) = {%s}\n" %(self.rule2.id, ', '.join([str(e) for e in rule2_first]))
-    string += "first(Rule-%d) ∩ first(Rule-%d): {%s}\n" % (self.rule1.id, self.rule2.id, ', '.join([str(e) for e in rule1_first.intersection(rule2_first)]))
+    string += "first(Rule-%d) = {%s}\n" %(self.rule1.id, ', '.join(sorted([str(e) for e in rule1_first])))
+    string += "first(Rule-%d) = {%s}\n" %(self.rule2.id, ', '.join(sorted([str(e) for e in rule2_first])))
+    string += "first(Rule-%d) ∩ first(Rule-%d): {%s}\n" % (self.rule1.id, self.rule2.id, ', '.join(sorted([str(e) for e in rule1_first.intersection(rule2_first)])))
 
     return string
 
@@ -74,6 +111,15 @@ class FirstFollowConflict(Conflict):
     self.N = N
     self.firstN = firstN
     self.followN = followN
+
+  def toJson(self):
+    x = [
+      ('type', '"FirstFirstConflict"'),
+      ('first(%s)' % (self.N), '[%s]' % (', '.join(sorted([str(e) for e in self.firstN])))),
+      ('follow(%s)' % (self.N), '[%s]' % (', '.join(sorted([str(e) for e in self.followN])))),
+      ('first(%s) ∩ follow(%s)' % (self.N, self.N), '[%s]' % (', '.join(sorted([str(z) for z in self.firstN.intersection(self.followN)]))))
+    ]
+    return '{\n%s\n}' % (',\n'.join(['  "%s": %s' % (k,v) for k,v in x]))
 
   def __str__( self ):
     string = ' -- FIRST/FOLLOW conflict --\n'
--- test/HermesTest.py
+++ test/HermesTest.py
@@ -29,6 +29,16 @@ class HermesFollowSetTest(HermesTest):
     self.assertEqual(self.actual, self.expected, \
         'Follow set for nonterminal %s in test %s does not match expected value\n\nexpected: %s\nactual:%s' % ( self.nonterminal, self.testCaseDir, self.expected, self.actual))
 
+class HermesConflictTest(HermesTest):
+
+  def __init__(self, testCaseDir=None, nonterminal=None, expected=None, actual=None):
+    super().__init__()
+    self.__dict__.update(locals())
+    self.maxDiff = None
+
+  def runTest(self):
+    self.assertEqual(self.actual, self.expected, 'expected conflicts to match')
+
 class HermesParseTest:
   pass
 
@@ -38,7 +48,7 @@ class HermesAbstractSyntaxTreeTest:
 def load_tests(loader, tests, pattern):
   testsDirectory = os.path.join(directory, 'grammar')
   suite = unittest.TestSuite()
-  jsonify = lambda arg:'{\n%s\n}' % (',\n'.join(['  "%s": [%s]' % (nt, ', '.join(['"'+z+'"' for z in theSet])) for nt, theSet in arg.items()]))
+  jsonifySets = lambda arg:'{\n%s\n}' % (',\n'.join(['  "%s": [%s]' % (nt, ', '.join(['"'+z+'"' for z in theSet])) for nt, theSet in arg.items()]))
   for grammarTest in os.listdir(testsDirectory):
     try:
       int(grammarTest)
@@ -56,6 +66,22 @@ def load_tests(loader, tests, pattern):
       if isinstance(k, NonTerminal):
         grammarFollow[k.string] = set(map(lambda x: x.string, v))
 
+    path = os.path.join(testDirectory, 'conflicts.json')
+    if os.path.exists(path):
+      contents = open(path).read()
+      if len(contents):
+        for k,v in expected.items():
+          suite.addTest(HermesConflictTest(testDirectory, k, contents, '\n'.join([x.toJson() for x in grammar.conflicts])))
+    else:
+      if len(grammar.conflicts):
+        fp = open(path, 'w')
+        fp.write('\n'.join([x.toJson() for x in grammar.conflicts]))
+        fp.close()
+      else:
+        fp = open(path, 'w')
+        fp.close()
+      print('generated %s/conflicts.json (%d conflicts)' % (path, len(grammar.conflicts)))
+
     path = os.path.join(testDirectory, 'first.json')
     if os.path.exists(path):
       contents = open(path).read()
@@ -64,12 +90,17 @@ def load_tests(loader, tests, pattern):
         for k,v in expected.items():
           suite.addTest(HermesFirstSetTest(testDirectory, k, set(expected[k]), grammarFirst[k]))
     else:
-      for k,v in grammarFirst.items():
-        grammarFirst[k] = list(v)
-      fp = open(path, 'w')
-      fp.write(jsonify(grammarFirst))
-      fp.close()
-      print('generated %s/first.json' % (path))
+      if len(grammar.conflicts):
+        fp = open(path, 'w')
+        fp.close()
+        print('generated %s/first.json (empty file because of conflicts)' % (path))
+      else:
+        for k,v in grammarFirst.items():
+          grammarFirst[k] = list(v)
+        fp = open(path, 'w')
+        fp.write(jsonifySets(grammarFirst))
+        fp.close()
+        print('generated %s/first.json' % (path))
 
     path = os.path.join(testDirectory, 'follow.json')
     if os.path.exists(path):
@@ -79,11 +110,16 @@ def load_tests(loader, tests, pattern):
         for k,v in expected.items():
           suite.addTest(HermesFollowSetTest(testDirectory, k, set(expected[k]), grammarFollow[k]))
     else:
-      for k,v in grammarFollow.items():
-        grammarFollow[k] = list(v)
-      fp = open(path, 'w')
-      fp.write(jsonify(grammarFollow))
-      fp.close()
-      print('generated %s/follow.json' % (path))
+      if len(grammar.conflicts):
+        fp = open(path, 'w')
+        fp.close()
+        print('generated %s/follow.json (empty file because of conflicts)' % (path))
+      else:
+        for k,v in grammarFollow.items():
+          grammarFollow[k] = list(v)
+        fp = open(path, 'w')
+        fp.write(jsonifySets(grammarFollow))
+        fp.close()
+        print('generated %s/follow.json' % (path))
 
   return suite
