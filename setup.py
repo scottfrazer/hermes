8b359bf8964c935aa36d247f3ca4c296d312b133
Scott Frazer <scott.d.frazer@gmail.com>
2012-09-21 10:32:03 -0400

    1.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ distribute_setup.use_setuptools()
 import os
 from setuptools import setup, find_packages
 
-version = '1.0.1'
+version = '1.0.2'
 long_description = 'LL(1) parser generator with expression parsing support' 
 
 setup(
