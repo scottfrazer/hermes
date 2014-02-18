bf5452a14cfe8c0ef6c219899068caa68d3612ba
Scott Frazer <sfrazer@broadinstitute.org>
2014-02-18 12:16:10 -0500

    Removing old test directory

--- test/cases/grammar/bootstrap.sh
+++
@@ -1,5 +0,0 @@
-for file in *
-do
-  rm $file/grammar.zgr 2>/dev/null
-  ln -s ../../../grammars/$file.zgr $file/grammar.zgr 2>/dev/null
-done
--- test/cases/parsing/reset.sh
+++
@@ -1,4 +0,0 @@
-for file in *
-do
-  rm $file/parsetree $file/ast 2>/dev/null
-done
