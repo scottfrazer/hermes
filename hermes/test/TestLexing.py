89a1ee466b4da6e1b67e8feff96f6cbc46ad6d2b
Scott Frazer <scott.d.frazer@gmail.com>
2014-07-25 08:48:08 -0400

    Travis test

--- hermes/test/TestLexing.py
+++ hermes/test/TestLexing.py
@@ -43,7 +43,7 @@ def lex_c(test_dir):
         shutil.copy(source_file, tmp_dir)
         CodeGenerator().generate(grammar, 'c', directory=tmp_dir, add_main=True)
         c_files = list(filter(lambda x: x.endswith('.c'), os.listdir(tmp_dir)))
-        command = 'gcc -o lexer -lpcre {sources} -g -Wall -pedantic -ansi -std=c99 2>&1'.format(sources=' '.join(c_files))
+        command = 'gcc -o lexer -I/usr/local/include -I/usr/include -L/usr/local/lib -L/usr/lib -lpcre {sources} -g -Wall -pedantic -ansi -std=c99 2>&1'.format(sources=' '.join(c_files))
         subprocess.check_call(command, cwd=tmp_dir, shell=True, stderr=None)
         command = './lexer tokens source'
         return subprocess.check_output(command, shell=True, stderr=None, cwd=tmp_dir).decode('utf-8').strip()
