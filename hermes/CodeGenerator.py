63051d9d27869fc5c9f0bade51b9e0f2c0497dea
Scott Frazer <scott.d.frazer@gmail.com>
2015-02-18 08:24:02 -0500

    refactoring

--- hermes/CodeGenerator.py
+++ hermes/CodeGenerator.py
@@ -34,104 +34,76 @@ def underscore_to_camelcase(value):
 #####
 
 class GrammarTemplate:
-  def __init__(self):
-    super().__init__()
-    self.__dict__.update(locals())
-  def render(self):
-    kwargs = {k: v for k, v in self.__dict__.items() if k not in ['self']}
-    code = loader.render(
-      self.template,
-      **kwargs
-    )
-    return remove_blank_lines(code)
-  def write(self):
-    out_file_path = self.get_filename()
-    try:
-      os.makedirs(os.path.dirname(out_file_path))
-    except FileExistsError:
-      pass
-    with open(out_file_path, 'w') as fp:
-      fp.write(self.render())
-    return out_file_path
+    def __init__(self):
+        super().__init__()
+        self.__dict__.update(locals())
+    def render(self):
+        kwargs = {k: v for k, v in self.__dict__.items() if k not in ['self']}
+        code = loader.render(
+            self.template,
+            **kwargs
+        )
+        return remove_blank_lines(code)
+    def write(self):
+        out_file_path = self.get_filename()
+        try:
+            os.makedirs(os.path.dirname(out_file_path))
+        except FileExistsError:
+            pass
+        with open(out_file_path, 'w') as fp:
+            fp.write(self.render())
+        return out_file_path
 
 class PythonTemplate(GrammarTemplate):
-  template = 'python/Template.py.tpl'
-  def get_filename(self):
-    return os.path.join(self.directory, '{0}_parser.py'.format(self.grammar.name))
+    template = 'python/Template.py.tpl'
+    def get_filename(self):
+        return os.path.join(self.directory, '{0}_parser.py'.format(self.grammar.name))
 
 class JavaTemplate(GrammarTemplate):
-  filename = 'Parser.java'
-  template = 'java/Template.java.tpl'
-  def get_filename(self):
-    prefix = underscore_to_camelcase(self.grammar.name)
-    return os.path.join(self.directory, self.java_package_to_path(), prefix + self.filename)
-  def java_package_to_path(self):
-    return '/'.join(self.java_package.split('.')) if self.java_package else ''
-  def render(self, **kwargs):
-    self.prefix = underscore_to_camelcase(self.grammar.name)
-    return super().render()
+    template = 'java/Template.java.tpl'
+    def get_filename(self):
+        prefix = underscore_to_camelcase(self.grammar.name)
+        return os.path.join(self.directory, self.java_package_to_path(), prefix + "Parser.java")
+    def java_package_to_path(self):
+        return '/'.join(self.java_package.split('.')) if self.java_package else ''
+    def render(self, **kwargs):
+        self.prefix = underscore_to_camelcase(self.grammar.name)
+        return super().render()
 
 class CTemplate(GrammarTemplate):
-  def get_filename(self):
-    return os.path.join(self.directory, self.filename)
-  def render(self, **kwargs):
-    self.prefix = self.grammar.name + '_'
-    return super().render()
+    def get_filename(self):
+        return os.path.join(self.directory, self.filename)
+    def render(self, **kwargs):
+        self.prefix = self.grammar.name + '_'
+        return super().render()
 
 class CSource(CTemplate):
-  template = 'c/template.c.tpl'
-  def get_filename(self):
-    return os.path.join(self.directory, self.grammar.name.lower() + '_parser.c')
+    template = 'c/template.c.tpl'
+    def get_filename(self):
+        return os.path.join(self.directory, self.grammar.name.lower() + '_parser.c')
 
 class CHeader(CTemplate):
-  template = 'c/template.h.tpl'
-  def get_filename(self):
-    return os.path.join(self.directory, self.grammar.name.lower() + '_parser.h')
+    template = 'c/template.h.tpl'
+    def get_filename(self):
+        return os.path.join(self.directory, self.grammar.name.lower() + '_parser.h')
 
 class JavascriptTemplate(GrammarTemplate):
-  def get_filename(self):
-    return os.path.join(self.directory, self.filename)
-  def render(self, **kwargs):
-    self.prefix = self.grammar.name + '_'
-    return super().render()
-
-class JavascriptAllTemplate(JavascriptTemplate):
-  template = 'javascript/all.js.tpl'
-  def get_filename(self):
-    return os.path.join(self.directory, self.grammar.name.lower() + '_parser.js')
-
-class PythonTemplateFactory:
-  def create(self, **kwargs):
-    return [PythonTemplate()]
-
-class JavaTemplateFactory:
-  def create(self, **kwargs):
-    return [JavaTemplate()]
-
-class CTemplateFactory:
-  def create(self, **kwargs):
-    return [CSource(), CHeader()]
-
-class JavascriptTemplateFactory:
-  def create(self, **kwargs):
-    return [JavascriptAllTemplate()]
+    template = 'javascript/template.js.tpl'
+    def get_filename(self):
+        return os.path.join(self.directory, self.grammar.name.lower() + '_parser.js')
+    def render(self, **kwargs):
+        self.prefix = self.grammar.name + '_'
+        return super().render()
 
 class CodeGenerator:
     templates = {
-        'python': PythonTemplateFactory,
-        'c': CTemplateFactory,
-        'java': JavaTemplateFactory,
-        'javascript': JavascriptTemplateFactory
+        'python': [PythonTemplate()],
+        'c': [CSource(), CHeader()],
+        'java': [JavaTemplate()],
+        'javascript': [JavascriptTemplate()]
     }
 
-    def get_template_factory(self, language):
-        for (template_language, template_class) in self.templates.items():
-            if template_language == language:
-                return template_class()
-        raise Exception('Invalid language: ' + language)
-
     def generate_internal(self, grammar):
-        template_factory = self.get_template_factory('python')
         args = {
             'grammar': grammar,
             'language': 'python',
@@ -140,20 +112,18 @@ class CodeGenerator:
         }
 
         code = ''
-        templates = template_factory.create(**args)
-        for template in templates:
+        for template in self.templates['python']:
             template.__dict__.update(args)
             code += template.render()
             code += '\n'
         return code
 
     def generate(self, grammar, language, directory='.', add_main=False, java_package=None, nodejs=False):
-        template_factory = self.get_template_factory(language)
+        if language not in self.templates:
+            raise Exception('Invalid language: ' + language)
         args = locals()
         del args['self']
         args['lexer'] = grammar.lexers[language] if language in grammar.lexers else None
-        args['python_internal'] = False
-        templates = template_factory.create(**args)
-        for template in templates:
+        for template in self.templates[language]:
             template.__dict__.update(args)
             template.write()
