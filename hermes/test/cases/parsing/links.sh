ad5d4978949638953a02c4ab96e60aeda9aecc5f
Scott Frazer <scott.d.frazer@gmail.com>
2015-03-07 09:57:07 -0500

    Testing case sensitivity in terminal, nonterminal, ASTs

--- hermes/test/cases/parsing/links.sh
+++
@@ -1,8 +0,0 @@
-for link in $(find . | grep '.zgr')
-do
-  filename=$(basename $(readlink -f $link))
-  dirname=$(dirname $link)
-  echo $link, $filename, $dirname
-  rm $link
-  ln -s ../../../grammars/parsing/$filename $dirname/grammar.zgr
-done
--- hermes/test/cases/parsing/reset.sh
+++
@@ -1,4 +0,0 @@
-for file in *
-do
-  rm $file/parse_tree $file/ast 2>/dev/null
-done
